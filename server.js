c0043850b5f9ffaaf21597c0388051cc2520404a
VegardGronas <92518188+VegardGronas@users.noreply.github.com>
2021-11-16 23:02:50 +0100

    Added error handler middleware

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = require('./modules/router');
+const errorHandler = require('./modules/middleware');
 const server = express();
 const PORT = process.env.PORT || 8080;
 server.set("PORT", PORT);
@@ -7,6 +8,7 @@ server.set("PORT", PORT);
 server.use(express.static('public'));
 server.use(express.json());
 server.use(router);
+server.use(errorHandler);
 
 server.listen(server.get('PORT'), function(){
     console.log("server running", server.get("PORT"));
